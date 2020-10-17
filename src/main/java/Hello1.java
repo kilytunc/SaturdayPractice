9766652b08751d4e7ed268b1286d57d4999cf206
kilytunc <65714537+kilytunc@users.noreply.github.com>
2020-10-17 11:41:41 +0200

    Update Hello1.java

--- src/main/java/Hello1.java
+++ src/main/java/Hello1.java
@@ -1,6 +1,9 @@
 public class Hello1 {
     public static void main(String[] args) {
         System.out.println("initial code");  // first code - first class
+        
+        
+        // change some codes here and make it
 
     }
 }
